70cb23bf90514cb0461a0c89fcc6c0ac09872695
Saurabh Jain <98027861+Emp0r0s@users.noreply.github.com>
2023-08-11 03:05:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 # Get the Azure SQL Database connection string from environment variables
 connection_string = os.environ.get('SQLCONNSTR_mydbconnection')
-
 # Function to establish a database connection
 def get_db_connection():
     try:
@@ -21,7 +20,7 @@ def hello():
     return "Hello World"
 
 
-@app.route('/get_data', methods=['GET'])
+@app.route('/data', methods=['GET'])
 def get_data():
     try:
         connection = get_db_connection()
@@ -30,27 +29,38 @@ def get_data():
             query = "SELECT * FROM items"
             cursor.execute(query)
             data = cursor.fetchall()
+
+            # Convert rows to list of dictionaries
+            result = [{'Name': row[0], 'Qty': row[1]} for row in data]
+
             cursor.close()
             connection.close()
-            return jsonify(data)
+
+            return jsonify(result)  # jsonify the list of dictionaries
         else:
             return "Unable to connect to the database", 500
     except Exception as e:
         return "An error occurred: " + str(e), 500
 
-@app.route('/put_data', methods=['PUT'])
+@app.route('/data', methods=['PUT'])
 def put_data():
     try:
         connection = get_db_connection()
         if connection:
             cursor = connection.cursor()
+            
             # Extract data from request JSON
             data = request.json
-            query = "INSERT INTO items (column1, column2) VALUES (?, ?)"
-            cursor.execute(query, data['value1'], data['value2'])
+            value1 = data.get('Name')  
+            value2 = data.get('Qty')  
+            
+            query = "INSERT INTO items (Name, Qty) VALUES (?, ?)"
+            cursor.execute(query, (value1, value2))
             connection.commit()
+            
             cursor.close()
             connection.close()
+            
             return "Data inserted successfully"
         else:
             return "Unable to connect to the database", 500
